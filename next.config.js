1afb3e0101e9ba18bb40168456a43895d2d17196
DmytroZhytnik <dmytro.zhytnik@cengage.com>
2020-08-22 18:24:01 +0300

    configs.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 module.exports = {
-  webpack: (config, {webpack}) => {
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
     return config;
   },
