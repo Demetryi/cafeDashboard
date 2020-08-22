25d252961a5d3c56f40a5980a96250cd62ba166a
DmytroZhytnik <dmytro.zhytnik@cengage.com>
2020-08-22 18:39:22 +0300

    removed config.

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-module.exports = {
-  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    return config;
-  },
-};
